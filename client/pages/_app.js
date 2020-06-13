3852fcf6e737b3176d43c22a29f3302f3e94e800
Jeonghoon-Lee <hoon22.lee@gmail.com>
2020-06-13 14:55:54 -0400

    created deployment files and updated DB connection URL

--- client/pages/_app.js
+++ client/pages/_app.js
@@ -19,7 +19,7 @@ AppComponent.getInitialProps = async appContext => {
   if (appContext.Component.getInitialProps) {
     pageProps = await appContext.Component.getInitialProps(appContext.ctx);
   }
-  console.log(pageProps);
+  // console.log(pageProps);
 
   return {
     pageProps,
