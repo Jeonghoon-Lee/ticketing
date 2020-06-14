79a2af0677a6bf9df1887ba028759a55fda19b31
Jeonghoon-Lee <hoon22.lee@gmail.com>
2020-06-13 23:15:16 -0400

    created service for ticket create/update/fetch

--- tickets/src/app.ts
+++ tickets/src/app.ts
@@ -2,7 +2,12 @@ import express, { Request, Response } from 'express';
 import 'express-async-errors';
 import { json } from 'body-parser';
 import cookieSession from 'cookie-session';
-import { errorHandler, NotFoundError } from '@jhtickets/common';
+import { errorHandler, NotFoundError, currentUser } from '@jhtickets/common';
+
+import { createTicketRouter } from './routes/new';
+import { showTicketRouter } from './routes/show';
+import { indexTicketRouter } from './routes/index';
+import { updateTicketRouter } from './routes/update';
 
 const app = express();
 // for ingress-iginx proxy
@@ -15,6 +20,12 @@ app.use(
     secure: process.env.NODE_ENV !== 'test',
   })
 );
+app.use(currentUser);
+
+app.use(createTicketRouter);
+app.use(showTicketRouter);
+app.use(indexTicketRouter);
+app.use(updateTicketRouter);
 
 // app.all('*', async (req, res, next) => {
 //   next(new NotFoundError());
