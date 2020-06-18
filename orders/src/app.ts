a093a018db9a3d7e6756dbae15b5b8be674c5149
Jeonghoon-Lee <hoon22.lee@gmail.com>
2020-06-18 17:15:52 -0400

    created order service structure

---
+++ orders/src/app.ts
@@ -0,0 +1,42 @@
+import express, { Request, Response } from 'express';
+import 'express-async-errors';
+import { json } from 'body-parser';
+import cookieSession from 'cookie-session';
+import { errorHandler, NotFoundError, currentUser } from '@jhtickets/common';
+
+import { createTicketRouter } from './routes/new';
+import { showTicketRouter } from './routes/show';
+import { indexTicketRouter } from './routes/index';
+import { updateTicketRouter } from './routes/update';
+
+const app = express();
+// for ingress-iginx proxy
+app.set('trust proxy', true);
+
+app.use(json());
+app.use(
+  cookieSession({
+    signed: false,
+    secure: process.env.NODE_ENV !== 'test',
+  })
+);
+app.use(currentUser);
+
+app.use(createTicketRouter);
+app.use(showTicketRouter);
+app.use(indexTicketRouter);
+app.use(updateTicketRouter);
+
+// app.all('*', async (req, res, next) => {
+//   next(new NotFoundError());
+// });
+
+// same as above
+// using 'express-async-errors'
+app.all('*', async (req, res) => {
+  throw new NotFoundError();
+});
+
+app.use(errorHandler);
+
+export { app };
