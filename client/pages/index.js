08522f3033b10b1d9b4e900700e0f28f5284bf02
Jeonghoon-Lee <hoon22.lee@gmail.com>
2020-07-05 22:48:54 -0400

    added the create-ticket page  and updated main page

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,16 +1,41 @@
-import buildClient from '../api/build-client';
+// import buildClient from '../api/build-client';
 
-const LandingPage = ({ currentUser }) => {
-  return <h1>{currentUser ? 'You are signed in' : 'You are NOT signed in'}</h1>;
+const LandingPage = ({ currentUser, tickets }) => {
+  const ticketList = tickets.map(ticket => {
+    return (
+      <tr key={ticket.id}>
+        <td>{ticket.title}</td>
+        <td>{ticket.price}</td>
+      </tr>
+    );
+  });
+  return (
+    <div>
+      <h1>Tickets</h1>
+      <table className="table">
+        <thead>
+          <tr>
+            <th>Title</th>
+            <th>Price</th>
+          </tr>
+        </thead>
+        <tbody>{ticketList}</tbody>
+      </table>
+    </div>
+  );
 };
 
 // run during the server-side rendering
-LandingPage.getInitialProps = async context => {
-  console.log('LANDING PAGE');
-  const client = buildClient(context);
-  const { data } = await client.get('/api/users/currentuser');
+LandingPage.getInitialProps = async (context, client, currentUser) => {
+  const { data } = await client.get('/api/tickets');
 
-  return data;
+  return { tickets: data };
+  // console.log('LANDING PAGE');
+  // const client = buildClient(context);
+  // const { data } = await client.get('/api/users/currentuser');
+
+  // return data;
+
   // using buildClient instead of the following code
   //
   // if (typeof window === 'undefined') {
