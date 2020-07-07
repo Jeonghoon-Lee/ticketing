a1ede2d6255abbdf324744366879a7cf8bdf0a06
Jeonghoon-Lee <hoon22.lee@gmail.com>
2020-07-06 23:18:27 -0400

    completed client module

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,11 +1,18 @@
 // import buildClient from '../api/build-client';
 
+import Link from 'next/link';
+
 const LandingPage = ({ currentUser, tickets }) => {
   const ticketList = tickets.map(ticket => {
     return (
       <tr key={ticket.id}>
         <td>{ticket.title}</td>
         <td>{ticket.price}</td>
+        <td>
+          <Link href="/tickets/[ticketId]" as={`/tickets/${ticket.id}`}>
+            <a>View</a>
+          </Link>
+        </td>
       </tr>
     );
   });
@@ -17,6 +24,7 @@ const LandingPage = ({ currentUser, tickets }) => {
           <tr>
             <th>Title</th>
             <th>Price</th>
+            <th>Link</th>
           </tr>
         </thead>
         <tbody>{ticketList}</tbody>
@@ -27,9 +35,14 @@ const LandingPage = ({ currentUser, tickets }) => {
 
 // run during the server-side rendering
 LandingPage.getInitialProps = async (context, client, currentUser) => {
+  // if (!client) return { tickets: [] };
+
   const { data } = await client.get('/api/tickets');
 
   return { tickets: data };
+
+  // return { tickets: [] };
+
   // console.log('LANDING PAGE');
   // const client = buildClient(context);
   // const { data } = await client.get('/api/users/currentuser');
